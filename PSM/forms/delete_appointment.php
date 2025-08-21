298745d7f1ba1654ddfb6f5f54d838b6b48bbdcd
Atharva Dhore <163399333+ASD21191530s@users.noreply.github.com>
2025-08-22 02:26:47 +0530

    Delete PSM directory

--- PSM/forms/delete_appointment.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("connect2.php");
-
-$id = $_POST['id'];
-
-$sql = "DELETE FROM appointment WHERE Aid = '$id'";
-
-echo mysqli_query($conn, $sql) ? "Appointment deleted." : "Failed to delete appointment.";
-?>
--- PSM/forms/delete_payment.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("connect2.php");
-
-$id = $_POST['id'];
-
-$sql = "DELETE FROM payment WHERE pay_id = '$id'";
-
-echo mysqli_query($conn, $sql) ? "Payment deleted." : "Failed to delete payment.";
-?>
